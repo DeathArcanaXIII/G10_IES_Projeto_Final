f279604c4d9352f0c3fea02267730417ca6b4d69
DeathArcanaXIII <120216256+DeathArcanaXIII@users.noreply.github.com>
2023-08-20 14:47:56 -0300

    Initial commit

---
+++ Aprovacao.java
@@ -0,0 +1,96 @@
+import java.util.*; 
+public class Aprovacao{
+    private boolean statusAprovacao;
+    private boolean statusSeguranca;
+    private boolean statusLimpeza;
+    private boolean statusPermissaoBombeiros;
+    private boolean statusLuzSom;
+    private boolean statusProjetorTMS;
+    private boolean statusTela;
+
+    public Aprovacao() {
+        // Configuração inicial dos status
+        statusAprovacao = false;
+        statusSeguranca = false;
+        statusLimpeza = false;
+        statusPermissaoBombeiros = false;
+        statusLuzSom = false;
+        statusProjetorTMS = false;
+        statusTela = false;
+    }
+
+    public boolean isStatusAprovacao() {
+        return statusAprovacao;
+    }
+
+    public void setStatusAprovacao(boolean statusAprovacao) {
+        this.statusAprovacao = statusAprovacao;
+    }
+
+    public boolean isStatusSeguranca() {
+        return statusSeguranca;
+    }
+
+    public void setStatusSeguranca(boolean statusSeguranca) {
+        this.statusSeguranca = statusSeguranca;
+    }
+
+    public boolean isStatusLimpeza() {
+        return statusLimpeza;
+    }
+
+    public void setStatusLimpeza(boolean statusLimpeza) {
+        this.statusLimpeza = statusLimpeza;
+    }
+
+    public boolean isStatusPermissaoBombeiros() {
+        return statusPermissaoBombeiros;
+    }
+
+    public void setStatusPermissaoBombeiros(boolean statusPermissaoBombeiros) {
+        this.statusPermissaoBombeiros = statusPermissaoBombeiros;
+    }
+
+    public boolean isStatusLuzSom() {
+        return statusLuzSom;
+    }
+
+    public void setStatusLuzSom(boolean statusLuzSom) {
+        this.statusLuzSom = statusLuzSom;
+    }
+
+    public boolean isStatusProjetorTMS() {
+        return statusProjetorTMS;
+    }
+
+    public void setStatusProjetorTMS(boolean statusProjetorTMS) {
+        this.statusProjetorTMS = statusProjetorTMS;
+    }
+
+    public boolean isStatusTela() {
+        return statusTela;
+    }
+
+    public void setStatusTela(boolean statusTela) {
+        this.statusTela = statusTela;
+    }
+
+    public void acessarSala() {
+       
+    }
+    public static void main(String[] args) {
+        Aprovacao aprovacao = new Aprovacao();
+
+        aprovacao.setStatusSeguranca(true);
+        aprovacao.setStatusLimpeza(true);
+        aprovacao.setStatusLuzSom(true);
+        aprovacao.setStatusProjetorTMS(true);
+        aprovacao.setStatusTela(true);
+        aprovacao.acessarSala();
+
+        // Alterar status de aprovação
+        aprovacao.setStatusAprovacao(true);
+
+        System.out.println("Status de aprovação: " + aprovacao.isStatusAprovacao());
+    }
+}
---
+++ Sala.java
@@ -0,0 +1,266 @@
+import java.util.*;
+
+/**
+ * Escreva uma descrição da classe Sala aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+
+public class Sala 
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int[] checkList = new int[8];
+
+    /**
+     * Construtor para objetos da classe Sala
+     */
+    public Sala()
+    {
+        // inicializa variáveis de instância
+        for(int i = 0; i < 8; i++){
+            checkList[i] = 0;
+        }
+    }
+
+    /**
+     Recebe o valor "0" para "Reprovado", e o valor "1" para "Aprovado". 
+     */
+    public void setStatusSeguranca(int status)
+    {
+        if(status != 0 && status!= 1)
+        {
+            System.out.println("INSIRA UM VALOR VÁLIDO!");
+        }
+        else
+        {
+            checkList[0] = status;
+        }
+    }
+    
+    /**
+     Recebe o valor "0" para "Reprovado", e o valor "1" para "Aprovado". 
+     */
+    public void setStatusLimpeza(int status)
+    {
+        if(status != 0 && status!= 1)
+        {
+            System.out.println("INSIRA UM VALOR VÁLIDO!");
+        }
+        else
+        {
+            checkList[1] = status;
+        }
+    }
+    
+    /**
+     Recebe o valor "0" para "Reprovado", e o valor "1" para "Aprovado". 
+     */
+    public void setStatusBombeiros(int status)
+    {
+        if(status != 0 && status!= 1)
+        {
+            System.out.println("INSIRA UM VALOR VÁLIDO!");
+        }
+        else
+        {
+            checkList[2] = status;
+        }
+    }
+    
+    /**
+     Recebe o valor "0" para "Reprovado", e o valor "1" para "Aprovado". 
+     */
+    public void setStatusLuzes(int status)
+    {
+        if(status != 0 && status!= 1)
+        {
+            System.out.println("INSIRA UM VALOR VÁLIDO!");
+        }
+        else
+        {
+            checkList[3] = status;
+        }
+    }
+    
+    /**
+     Recebe o valor "0" para "Reprovado", e o valor "1" para "Aprovado". 
+     */
+    public void setStatusSom(int status)
+    {
+        if(status != 0 && status!= 1)
+        {
+            System.out.println("INSIRA UM VALOR VÁLIDO!");
+        }
+        else
+        {
+            checkList[4] = status;
+        }
+    }
+    
+    /**
+     Recebe o valor "0" para "Reprovado", e o valor "1" para "Aprovado". 
+     */
+    public void setStatusTela(int status)
+    {
+        if(status != 0 && status!= 1)
+        {
+            System.out.println("INSIRA UM VALOR VÁLIDO!");
+        }
+        else
+        {
+            checkList[5] = status;
+        }
+    }
+    
+    /**
+     Recebe o valor "0" para "Reprovado", e o valor "1" para "Aprovado". 
+     */
+    public void setStatusProjetor(int status)
+    {
+        if(status != 0 && status!= 1)
+        {
+            System.out.println("INSIRA UM VALOR VÁLIDO!");
+        }
+        else
+        {
+            checkList[6] = status;
+        }
+    }
+    
+    /**
+     Recebe o valor "0" para "Reprovado", e o valor "1" para "Aprovado". 
+     */
+    public void setStatusTMS(int status)
+    {
+        if(status != 0 && status!= 1)
+        {
+            System.out.println("INSIRA UM VALOR VÁLIDO!");
+        }
+        else
+        {
+            checkList[7] = status;
+        }
+    }
+    
+    /**
+     Retorna "TRUE" caso funcionando corretamento, retorna "FALSE" caso necessite de manutenção. 
+     */
+    public boolean getStatusSeguranca()
+    {
+        if(checkList[0] == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
+    /**
+     Retorna "TRUE" caso funcionando corretamento, retorna "FALSE" caso necessite de manutenção. 
+     */
+    public boolean getStatusLimpeza()
+    {
+        if(checkList[1] == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
+    /**
+     Retorna "TRUE" caso funcionando corretamento, retorna "FALSE" caso necessite de manutenção. 
+     */
+    public boolean getStatusBombeiros()
+    {
+        if(checkList[2] == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
+    /**
+     Retorna "TRUE" caso funcionando corretamento, retorna "FALSE" caso necessite de manutenção. 
+     */
+    public boolean getStatusLuzes()
+    {
+        if(checkList[3] == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
+    /**
+     Retorna "TRUE" caso funcionando corretamento, retorna "FALSE" caso necessite de manutenção. 
+     */
+    public boolean getStatusSom()
+    {
+        if(checkList[4] == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
+    /**
+     Retorna "TRUE" caso funcionando corretamento, retorna "FALSE" caso necessite de manutenção. 
+     */
+    public boolean getStatusTela()
+    {
+        if(checkList[5] == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
+    /**
+     Retorna "TRUE" caso funcionando corretamento, retorna "FALSE" caso necessite de manutenção. 
+     */
+    public boolean getStatusProjetor()
+    {
+        if(checkList[6] == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    
+    /**
+     Retorna "TRUE" caso funcionando corretamento, retorna "FALSE" caso necessite de manutenção. 
+     */
+    public boolean getStatusTMS()
+    {
+        if(checkList[7] == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
+
